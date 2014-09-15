d2113c2318b52495448c59886583b1cc40962965
Mikhail Valkov <valkov.net@gmail.com>
2014-09-15 16:01:25 +0300

    Set the settings for identity client

--- Sources/IdentityClient/App_Start/AuthConfig.cs
+++ Sources/IdentityClient/App_Start/AuthConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-using IdentityClient.Models;
-
-namespace IdentityClient
+﻿namespace IdentityClient
 {
     public static class AuthConfig
     {
